583d791f5b7db14541eeab525268e2a3d2d0897b
x4d3 <git@xade.eu>
2015-12-08 17:33:46 +0000

    save image to database

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,115 +1,118 @@
 (function(window, document) {
-  var createGIFButton = document.querySelector('#create-gif'),
-    gifSource = document.querySelector('#GIFSource'),
-    gifType = document.querySelector('#GIFType'),
-    interval = document.querySelector("#interval"),
-    numFrames = document.querySelector("#numFrames"),
-    gifHeight = document.querySelector("#gifHeight"),
-    gifWidth = document.querySelector("#gifWidth"),
-    progressBar = document.querySelector("progress"),
-    text = document.querySelector('#gifText'),
-    fontWeight = document.querySelector('#fontWeight'),
-    fontSize = document.querySelector('#fontSize'),
-    fontFamily = document.querySelector('#fontFamily'),
-    fontColor = document.querySelector('#fontColor'),
-    textAlign = document.querySelector('#textAlign'),
-    textBaseline = document.querySelector('#textBaseline'),
-    sampleInterval = document.querySelector('#sampleInterval'),
-    numWorkers = document.querySelector('#numWorkers'),
-    saveAsFile = document.querySelector('#saveAsFile'),
-    gifshotImagePreview = document.querySelector('.gifshot-image-preview-section'),
-    placeholderDiv = document.querySelector('.placeholder-div'),
-    placeholderDivDimensions = document.querySelector('.placeholder-div-dimensions'),
-    gifshotCode = document.querySelector('.gifshot-code'),
-    gifshotCodeTemplate = document.querySelector('.gifshot-code-template'),
-    getSelectedOptions = function() {
-      return {
-        'gifWidth': +gifWidth.value,
-        'gifHeight': +gifHeight.value,
-        'images': gifSource.value === 'images' ? ['http://i.imgur.com/2OO33vX.jpg', 'http://i.imgur.com/qOwVaSN.png', 'http://i.imgur.com/Vo5mFZJ.gif'] : false,
-        'video': gifSource.value === 'video' ? ['example.mp4', 'example.ogv'] : false,
-        'interval': +interval.value,
-        'numFrames': +numFrames.value,
-        'text': text.value,
-        'fontWeight': fontWeight.value,
-        'fontSize': fontSize.value + 'px',
-        'fontFamily': fontFamily.value,
-        'fontColor': fontColor.value,
-        'textAlign': textAlign.value,
-        'textBaseline': textBaseline.value,
-        'sampleInterval': +sampleInterval.value,
-        'numWorkers': +numWorkers.value,
-      }
-    },
-    passedOptions,
-    bindEvents = function() {
-      createGIFButton.addEventListener('click', function(e) {
-        passedOptions = _.merge(_.clone(getSelectedOptions()), {
-          'progressCallback': function(captureProgress) {
-            //gifshotImagePreview.innerHTML = '';
-            placeholderDiv.classList.add('hidden');
-            progressBar.classList.remove('hidden');
-            progressBar.value = captureProgress;
-          }
-        });
-
-        var method = gifType.value === 'snapshot' ? 'takeSnapShot' : 'createGIF';
-
-        gifshot[method](passedOptions, function(obj) {
-          if (!obj.error) {
-            var image = obj.image;
-
-            var setImageCallBack = function(imageSrc){
-              progressBar.classList.add('hidden');
-              progressBar.value = 0;
-              animatedImage = document.createElement('img');
-              animatedImage.src = imageSrc;
-              gifshotImagePreview.appendChild(animatedImage);
-            };
-            
-            if(saveAsFile.checked){
-              ajax({
-                url:'/upload',
-                type:"POST",
-                data:JSON.stringify({image:image}),
-                contentType:"application/json; charset=utf-8",
-                dataType:"json",
-                success: function(result){
-                  setImageCallBack(result.imageSrc);
-                }
-              });
-              
-            }else{
-              setImageCallBack(image);
+    var createGIFButton = document.querySelector('#create-gif'),
+        gifType = document.querySelector('#GIFType'),
+        interval = document.querySelector("#interval"),
+        numFrames = document.querySelector("#numFrames"),
+        gifHeight = document.querySelector("#gifHeight"),
+        gifWidth = document.querySelector("#gifWidth"),
+        progressBar = document.querySelector("progress"),
+        text = document.querySelector('#gifText'),
+        fontWeight = document.querySelector('#fontWeight'),
+        fontSize = document.querySelector('#fontSize'),
+        fontFamily = document.querySelector('#fontFamily'),
+        fontColor = document.querySelector('#fontColor'),
+        textAlign = document.querySelector('#textAlign'),
+        textBaseline = document.querySelector('#textBaseline'),
+        sampleInterval = document.querySelector('#sampleInterval'),
+        numWorkers = document.querySelector('#numWorkers'),
+        saveAsFile = document.querySelector('#saveAsFile'),
+        gifshotImagePreview = $('.gifshot-image-preview-section'),
+        placeholderDiv = document.querySelector('.placeholder-div'),
+        placeholderDivDimensions = document.querySelector('.placeholder-div-dimensions'),
+        gifshotCode = document.querySelector('.gifshot-code'),
+        gifshotCodeTemplate = document.querySelector('.gifshot-code-template'),
+        getSelectedOptions = function() {
+            return {
+                'gifWidth': +gifWidth.value,
+                'gifHeight': +gifHeight.value,
+                'images': false,
+                'video': false,
+                'interval': +interval.value,
+                'numFrames': +numFrames.value,
+                'text': text.value,
+                'fontWeight': fontWeight.value,
+                'fontSize': fontSize.value + 'px',
+                'fontFamily': fontFamily.value,
+                'fontColor': fontColor.value,
+                'textAlign': textAlign.value,
+                'textBaseline': textBaseline.value,
+                'sampleInterval': +sampleInterval.value,
+                'numWorkers': +numWorkers.value,
             }
-          } else {
-            console.log('obj.error', obj.error);
-            console.log('obj.errorCode', obj.errorCode);
-            console.log('obj.errorMsg', obj.errorMsg);
-          }
-        });
-      }, false);
+        },
+        passedOptions,
+        bindEvents = function() {
+            createGIFButton.addEventListener('click', function(e) {
+                passedOptions = _.merge(_.clone(getSelectedOptions()), {
+                    'progressCallback': function(captureProgress) {
+                        placeholderDiv.classList.add('hidden');
+                        progressBar.classList.remove('hidden');
+                        progressBar.value = captureProgress;
+                    }
+                });
 
-    };
+                var method = gifType.value === 'snapshot' ? 'takeSnapShot' : 'createGIF';
 
-	var video = document.querySelector("#videoElement");
-	 
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-	 
-	if (navigator.getUserMedia) {       
-		navigator.getUserMedia({video: true}, handleVideo, videoError);
-	}
-	 
-	function handleVideo(stream) {
-		video.src = window.URL.createObjectURL(stream);
-	}
-	 
-	function videoError(e) {
-		// do something
-	}
+                gifshot[method](passedOptions, function(obj) {
+                    if (!obj.error) {
+                        var image = obj.image;
+
+                        progressBar.classList.add('hidden');
+                        progressBar.value = 0;
+                        var container = $('<div>').addClass("col-md-3");
+                        var animatedImage = $('<img>').attr('src', image);
+                        container.append(animatedImage);
+                        var button = $('<button>').text('Create Link')
+						.addClass("btn btn-primary btn-block")
+						.click(function() {
+                            $.ajax({
+                                url: '/saveBase64Image',
+                                type: "POST",
+                                data: JSON.stringify({
+                                    image: image
+                                }),
+                                contentType: "application/json; charset=utf-8",
+                                dataType: "json",
+                                success: function(result) {
+                                    animatedImage.attr('src', result.imageSrc);
+                                    button.text('Copy Link').click(function() {
+                                        window.prompt("Copy to clipboard: Ctrl+C, Enter", window.location.href + result.imageSrc);
+                                    });
+                                }
+                            });
+                        });
+                        container.append(button);
+                        gifshotImagePreview.append(container);
+                    } else {
+                        console.log('obj.error', obj.error);
+                        console.log('obj.errorCode', obj.errorCode);
+                        console.log('obj.errorMsg', obj.errorMsg);
+                    }
+                });
+            }, false);
+
+        };
+
+    var video = document.querySelector("#videoElement");
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({
+            video: true
+        }, handleVideo, videoError);
+    }
+
+    function handleVideo(stream) {
+        video.src = window.URL.createObjectURL(stream);
+    }
+
+    function videoError(e) {
+        // do something
+    }
 
 
-		
-	
-  bindEvents();
+
+
+    bindEvents();
 }(window, document));
--- demo/server/server.js
+++ demo/server/server.js
@@ -1,31 +1,73 @@
 // server.js (Express 4.0)
-var express = require('express'), app = express(), path = require('path'), fs = require('fs-extra'), bodyParser = require('body-parser'), shortid = require('shortid');
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    fs = require('fs-extra'),
+    bodyParser = require('body-parser'),
+    shortid = require('shortid'),
+    url = require('url'),
+    pg = require('pg');
 // SERVER CONFIGURATION
-var IMAGE_DIR = path.join(__dirname, '/../g');
-fs.emptyDir(IMAGE_DIR, function (err) {
-  if (!err) console.log(IMAGE_DIR + " dir was cleared.")
-})
-
-
 app.use(express.static(__dirname + '/../')); // set the static files location
 app.set('port', (process.env.PORT || 7000));
 
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
 
-app.post('/upload', function(req, res) {
-  var imageName = shortid.generate() + ".gif";
-  var base64Data = req.body.image.replace(/^data:image\/gif;base64,/, "");
-  var targetPath =  path.join(IMAGE_DIR, imageName);
-  fs.writeFile(targetPath,  new Buffer(base64Data, 'base64'), function(err) {
-     if (err) throw err;
-     console.log('File Saved in: ' + targetPath);
-  });
+app.post('/saveBase64Image', function(req, res) {
+    var imageName = shortid.generate() + ".gif";
+    var base64Data = req.body.image.replace(/^data:image\/gif;base64,/, "");
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.error(err);
+			done();
+			res.status(500).json({ success: false, data: err});
+        } else {
+            client.query('insert into image values ($1, $2)', [imageName, new Buffer(base64Data, 'base64')],
+                function(err, writeResult) {
+					if (err) {
+						console.error(err);
+						res.status(500).json({ success: false, data: err});
+					}else{
+						res.json({imageSrc: "g/" + imageName});
+					}
+                });
+        }
+    });
 
-  res.send(JSON.stringify({
-    imageSrc : "g/" + imageName
-  }));
+
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
+
+
+app.get('/g/:fileName', function(request, response) {
+    var fileName = request.params.fileName;
+    console.log("get image: " + fileName);
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.error(err);
+            response.send("Error " + err);
+			return;
+        } 
+		client.query('SELECT data FROM image where name=($1)', [fileName], function(err, result) {
+			done();
+			if (err) {
+				console.error(err);
+				response.send("Error " + err);
+				return;
+			}
+			if (result.rows.length == 1) {
+				response.writeHead(200, {
+					'Content-Type': 'image/gif'
+				});
+				response.end(result.rows[0].data, 'binary');
+			} else {
+				response.send("Could not find image: " + fileName);
+			}
+		});
+    });
+})
